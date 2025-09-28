26e02be48399ff0b4d05454c6d5b0cc600a4ebb9
Zavier <85642176+ZavierLowe@users.noreply.github.com>
2025-09-28 11:16:30 -0400

    Added about me section and working on the contact me section

---
+++ src/app/components/AboutMe.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Image from 'next/image'
+
+const AboutMe = () => {
+  return (
+      <section className="   ">
+          <div id='divLineWrapper' className='px-5 max-w-fill  lg:px-[140px] '>
+      
+          <div className=' mx-auto max-w-7xl border-l border-[#C2C8D6]/50 border-r pb-24'>
+      
+          <div className="max-w-7xl mx-auto  md:pt-30">
+            <div className="pl-2 pr-2">
+              <div className="flex flex-col justify-center items-center gap-4 pr-0 md:px-10">
+
+                <p className="text-xs font-semibold uppercase text-[#C2C8D6] mt-16">get to know more </p>
+
+                <h2 className="font-bold text-5xl uppercase mt-2 text-[#772BE2]">About Me</h2>
+                
+                <div className="relative mt-16 md:mt-0">
+                  <div className="p-6 mt-7 bg-white border border-[#C2C8D6]/50 rounded-[28px] shadow-md hover:shadow-lg transition ">
+
+                    <div className="flex gap-8">
+                     <Image className="rounded-[24px]" alt="me" src={'/images/about-me-pic.png'} width={320} height={500}/>
+
+                     <div className="flex flex-col max-w-lg items-start space-y-4">
+
+                     <h3 className="uppercase font-bold">hey i’m zavier </h3>
+                     <p>You can call me Zav for short !</p>
+                     <p className="text-md">I combine design, coding, and behavioral science to help businesses grow smarter`an Email Developer with a sharp eye for crafting responsive, pixel-perfect HTML table emails—whether built from scratch or based on a reference design. I’m skilled in tools like Figma and Photoshop, and I’m experienced across platforms including Shopify, WordPress, Google Analytics, Litmus, Klaviyo, AMPScript, and Cursor AI.Outside of development, I’m passionate about art, music, and exploring the world. Traveling, staying active, and connecting with different cultures are core to who I am, I’m always curious, always learning.</p>
+                     </div>
+
+                    </div>
+                      
+
+                  </div>
+                
+      
+             
+              </div>
+      
+               
+               
+              </div>
+              
+      
+            </div>
+          </div>
+          </div>
+          </div>
+         
+      
+        
+        </section>
+  )
+}
+
+export default AboutMe
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar"
 import Projects from "./ProjectDetails";
 import Hero from "./components/Hero";
 import ProjectGrid from "./components/ProjectGrid";
+import AboutMe from "./components/AboutMe";
 
 export default function Home() {
   return (
@@ -21,47 +22,64 @@ export default function Home() {
       <ProjectGrid/>
       <div className=" border-1 border-[#C2C8D6]/50"></div>
 
-         <section className="   ">
-          <div id='divLineWrapper' className='px-5 max-w-fill  lg:px-[140px] '>
-      
-          <div className=' mx-auto max-w-7xl border-l border-[#C2C8D6]/50 border-r pb-24'>
-      
-          <div className="max-w-7xl mx-auto  md:pt-30">
-            <div className="pl-2 pr-2">
-              <div className="flex flex-col justify-center items-center gap-4 pr-0 md:px-10">
+       <AboutMe/>
+     <div className=" border-1 border-[#C2C8D6]/50"></div>
+       <section className="   ">
+               <div id='divLineWrapper' className='px-5 max-w-fill  lg:px-[140px] '>
+           
+               <div className=' mx-auto max-w-7xl border-l border-[#C2C8D6]/50 border-r pb-24'>
+           
+               <div className="max-w-7xl mx-auto  md:pt-30">
+                 <div className="pl-2 pr-2">
+                   <div className="flex flex-col justify-center  gap-4 pr-0 md:px-10">
+     
+                     <p className="text-xs font-semibold uppercase text-[#C2C8D6] ml-2 mt-16">want to learn more </p>
+     
+                     <h2 className="font-bold text-5xl uppercase mt-2">Contact Me</h2>
+                     
+                     <div className="relative mt-16 md:mt-0">
+                       <div className="p-6 mt-7 bg-white border border-[#C2C8D6]/50 rounded-[28px] shadow-md hover:shadow-lg transition cursor-pointer">
+     
+                         <div className="flex gap-8">
+                          <div className=" flex flex-col p-6 gap-5">
+                              <h3 className="uppercase font-bold max-w-xs">Curious on what I could do for your company? </h3>
+                          <p className="max-w-xs">Just shoot me an email and I’ll answer your questions promptly.
+                              Hope to hear from you soon!</p>
 
-                <p className="text-xs font-semibold uppercase text-[#C2C8D6] mt-24">get to know more </p>
+                              <div className="bg-[#772BE2] p-3 text-center rounded-sm max-w-sm">
+                                <p className="uppercase font-bold text-[#FFFBED] text-xl tracking-wide">zavier.lowe@gmail.com</p>
+                              </div>
 
-                <h2 className="font-bold text-5xl uppercase my-4">About Me</h2>
-                
-                <div className="relative mt-16 md:mt-0">
-                  <div className="p-4 bg-white border border-[#F4F4F4] rounded-lg shadow-md hover:shadow-lg transition cursor-pointer">
-
-                    <div>
-                     <Image src={} width={500} height={500}/>
-                    </div>
-                      
-
-                  </div>
-                
-      
-             
-              </div>
-      
-               
-               
-              </div>
+                          </div>
+                        
+     
+                          <div className="flex flex-col max-w-lg items-start space-y-4">
+     
+                          
+                          </div>
+     
+                         </div>
+                           
+     
+                       </div>
+                     
+           
+                  
+                   </div>
+           
+                    
+                    
+                   </div>
+                   
+           
+                 </div>
+               </div>
+               </div>
+               </div>
               
-      
-            </div>
-          </div>
-          </div>
-          </div>
-         
-      
-        
-        </section>
-   
+           
+             
+             </section>
     
    <NavBar/>
     </div>
